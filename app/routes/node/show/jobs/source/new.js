357b575de607f008e0645d1dc225e06aacbc23d0
Johan Andersson von Geijer <Johan.Andersson.von.Geijer@ub.gu.se>
2015-02-18 23:29:15 +0100

    DFLOW-90 Added template, route and translations for the job creation form.

--- app/routes/node/show/jobs/source/new.js
+++ app/routes/node/show/jobs/source/new.js
@@ -2,6 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
+    // get the model data from the upstream source form
     return this.modelFor('node.show.jobs.source');
+  },
+  actions: {
+    createJob: function(model) {
+      var that = this;
+      this.store.save('job', model).then(
+        // callback function for store to use in case of success
+        function(model) {
+          that.send('refreshModel'); // Refresh children of current model
+          that.transitionTo('node.show.jobs');
+        },
+        // callback function for store to use in case of failure
+        function(errorObject) {
+          that.controller.set('error', errorObject.error);
+        }
+      );
+    }
   }
 });

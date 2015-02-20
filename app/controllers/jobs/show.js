c202e9e54bca0e61c2025068d07b5e953832ab03
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2015-02-20 13:43:49 +0100

    DFLOW-79 Added button for starting a job

--- app/controllers/jobs/show.js
+++ app/controllers/jobs/show.js
@@ -5,5 +5,8 @@ export default Ember.ObjectController.extend({
   needs: ['application'],
   printURL: function(){
     return ENV.APP.serviceURL + '/assets/work_order/' + this.get('model.id') + '.pdf';
-  }.property('model.id')
+  }.property('model.id'),
+  startable: function(){
+    return this.get('model.status') === 'waiting_for_digitizing';
+  }.property('model.status')
 });
--- app/routes/jobs/show.js
+++ app/routes/jobs/show.js
@@ -3,5 +3,21 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('job', params.id);
+  },
+  actions: {
+    startJob: function(id){
+      var that = this;
+      this.store.find('job', id + '/digitizing_begin').then(
+        // Success function
+        function(model) {
+          that.refresh(id); // Refresh children of current model
+          //that.transitionTo('jobs.show');
+        },
+      // Failed function
+        function(errorObject) {
+          that.controller.set('error', errorObject.error);
+        }
+      );
+    }
   }
 });

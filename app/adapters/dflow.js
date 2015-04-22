63f6e56cba193943ed88584748662ff5cde5a535
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2015-04-22 16:25:08 +0200

    DFLOW-67 Add button and modal with status selection for removing job from quarantine

--- app/adapters/dflow.js
+++ app/adapters/dflow.js
@@ -5,6 +5,7 @@ export default Ember.Object.extend({
     treenode:  { path: 'api/treenodes'  },
     user: { path: 'api/users'},
     role: { path: 'api/config/role_list'},
+    status: { path: 'api/config/status_list'},
     source: { path: 'api/sources'},
     job: { path: 'api/jobs'}
   },
--- app/controllers/jobs/show.js
+++ app/controllers/jobs/show.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
   needs: ['application'],
+  statusSelectionBinding: 'controllers.application.statusSelection',
   startable: function(){
     return this.get('model.status') === 'waiting_for_digitizing';
   }.property('model.status'),

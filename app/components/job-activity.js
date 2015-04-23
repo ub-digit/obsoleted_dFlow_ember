198c4f99858b56eb66580db725a17f4abcc977a2
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2015-04-23 11:49:45 +0200

    Add comments

--- app/components/job-activity.js
+++ app/components/job-activity.js
@@ -2,10 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'tr',
+
+  // Returns a translated event name
   displayedEvent: Ember.computed('activity.event', function(){
     return Ember.I18n.t('activityevent.'+this.get('activity.event'));
   }),
 
+  // Returns a translated message if message begins with '_' or 'STATUS'
   displayedMessage: Ember.computed('activity.message', function(){
     if (this.get('activity.message').charAt(0) === '_') {
       var string = this.get('activity.message').slice(1);
@@ -18,6 +21,7 @@ export default Ember.Component.extend({
     }
   }),
 
+  // Formats date
   displayedDate: Ember.computed('activity.created_at', function(){
     return moment(this.get('activity.created_at')).format("YYYY-MM-DD hh:mm:ss");
   })

ebbb81aecc13feb25abc4a05e463d06bb6509ede
Johan Andersson von Geijer <Johan.Andersson.von.Geijer@ub.gu.se>
2015-04-28 18:19:32 +0200

    DFLOW-19: Modified the action so that values from dc form might be handled at all.

--- app/routes/node/show/jobs/source.js
+++ app/routes/node/show/jobs/source.js
@@ -15,10 +15,18 @@ export default Ember.Route.extend({
     var node = this.modelFor('node.show');
     return {treenode_id: node.id}; // Beginning with this data
   },
+  setupController: function(controller, model) {
+    controller.set('model', model);
+    controller.set('model.dc', {});
+  },
   actions: {
     fetchSource: function(model) {
       var that = this;
-      this.store.find('source', model.catalog_id, {name: model.source}).then(
+      if (model.source === 'dc') {
+        model.catalog_id = 'dc';
+      }
+      this.store.find('source', model.catalog_id, {name: model.source, dc: model.dc}).then(
+      //this.store.find('source', model.catalog_id, {name: model.source}).then(
         function(source_data){
           that.controller.set('model.title', source_data.title);
           that.controller.set('model.author', source_data.author);

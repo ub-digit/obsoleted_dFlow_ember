6692ad529c6da72df34a7a3e94f44db81715ca06
Johan Andersson von Geijer <Johan.Andersson.von.Geijer@ub.gu.se>
2015-04-28 11:31:45 +0200

    DFLOW-19: Added a computed property in order to show different form depending on which source is selected.

--- app/controllers/node/show/jobs/source.js
+++ app/controllers/node/show/jobs/source.js
@@ -2,5 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   needs: ['application'],
-  sourceSelectionBinding: 'controllers.application.sourceSelection'
+  sourceSelectionBinding: 'controllers.application.sourceSelection',
+
+  isDC: Ember.computed.equal('model.source', 'dc'),
 });

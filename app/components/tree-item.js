2f1498e041e853173edb14a8f7631c71d1bae1d6
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2015-05-11 13:58:46 +0200

    Add link to file items for download

--- app/components/tree-item.js
+++ app/components/tree-item.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
+import ENV from 'd-flow-ember/config/environment';
 
 export default Ember.Component.extend({
   tagName: 'div',
   isExpanded: false,
+  parentPath: '/',
 
   byteString: Ember.computed('item.size', function() {
     var size = this.get('item.size');
@@ -14,6 +16,31 @@ export default Ember.Component.extend({
     } while (size > 1024);
 
     return Math.max(size, 0.1).toFixed(1) + byteUnits[i];
+  }),
+
+  isImage: Ember.computed('item.name', function(){
+    return ['jpg', 'jpeg','tif', 'tiff', 'png', 'jp2'].contains(this.get('item.name').split('.').pop());
+  }),
+
+  isPdf: Ember.computed('item.name', function(){
+    return ['pdf'].contains(this.get('item.name').split('.').pop());
+  }),
+
+  isText: Ember.computed('item.name', function(){
+    return ['xml', 'txt'].contains(this.get('item.name').split('.').pop());
+  }),
+
+  isFile: Ember.computed('item.name', function(){
+    return !this.isImage && !this.isPdf && !this.isText;
+  }),
+
+  path: Ember.computed('parentPath', function(){
+    return this.get('parentPath') + this.get('item.name') + '/';
+  }),
+
+  fileUrl: Ember.computed('item', function() {
+    var token =  this.container.lookup('simple-auth-session:main').get('token');
+    return ENV.APP.serviceURL + '/assets/job_file/' + this.get('jobId') + '?file_dir=' + this.get('parentPath') + '&file_name=' + this.get('item.name') + '&token=' + token;
   }),
 
   actions: {

0f15a531aa67bfab5ea13ced95817cbb22b4a246
jkcarlsson85 <jkcarlsson85@gmail.com>
2016-06-22 15:19:43 +0200

    Add controller and link to queue page

--- app/routes/jobs/queue.js
+++ app/routes/jobs/queue.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(){
     return this.store.find('queue');
-  }
+  },
+
+  setupController: function(controller, model){
+    controller.set('model', model);
+  },
+
+  actions: {
+    refreshModel: function(){
+      this.refresh();
+    }
+   }
 });
